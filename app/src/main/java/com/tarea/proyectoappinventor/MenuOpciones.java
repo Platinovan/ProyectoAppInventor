156ec759d604ea0575163ddac14ff2023efc736c
Jose Luis <josl.cuevas27@gmail.com>
2021-11-30 01:17:54 -0600

    Agregando Fuentes

--- app/src/main/java/com/tarea/proyectoappinventor/MenuOpciones.java
+++ app/src/main/java/com/tarea/proyectoappinventor/MenuOpciones.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.widget.Toast;
-
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
