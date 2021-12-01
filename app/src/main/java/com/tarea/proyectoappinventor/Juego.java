b1cbc2f62a2ba513aa51daa58a668c65a071c29d
Jose Luis <josl.cuevas27@gmail.com>
2021-12-01 04:21:49 -0600

    Animaciones

--- app/src/main/java/com/tarea/proyectoappinventor/Juego.java
+++ app/src/main/java/com/tarea/proyectoappinventor/Juego.java
@@ -2,9 +2,20 @@ package com.tarea.proyectoappinventor;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.airbnb.lottie.LottieAnimationView;
 
 public class Juego extends AppCompatActivity {
+    //Declaracion de las variables
+    LottieAnimationView sprite;
+    String UID, USER, SCORE;
+    TextView puntaje, tiempo;
+    int contador = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +23,26 @@ public class Juego extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
         setContentView(R.layout.activity_juego);
+
+        //Inicializacion de la svariables
+        sprite = findViewById(R.id.Sprite);
+        puntaje = findViewById(R.id.Puntaje);
+        tiempo = findViewById(R.id.TiempoRestante);
+
+        //Se recuperan los valores enviados por la actvidad menu
+        Bundle intent = getIntent().getExtras();
+
+        UID = intent.getString("UID");
+        USER = intent.getString("USER");
+        SCORE = intent.getString("SCORE");
+
+        //Se setean los valores de contador y tiempo
+        puntaje.setText(SCORE);
+        sprite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
+
     }
 }
